5740c0b1a4e2ddcd0cc3dc5d6e6a91b628e85edf
Enzo1987Pavel <Enzo1987@mail.ru>
2022-04-25 06:01:20 +0300

    Edited project

--- funcs.py
+++ funcs.py
@@ -1,29 +1,39 @@
 import json
 
+candidate_list = []
+
 
 def load_candidates_from_json(path):
 	"""Загрузка списка кандидатов из файла"""
+	global candidate_list
 
 	with open(path, "r", encoding="utf-8") as candidates:
-		return json.load(candidates)
+		candidate_list = json.load(candidates)
+	return candidate_list
 
 
-def get_candidate(candidates, candidate_id):
+def get_candidate(candidate_id):
 	"""Возвращает одного кандидата по его id"""
-	for candidate in candidates:
+	for candidate in candidate_list:
 		if candidate["id"] == candidate_id:
 			return candidate
 
+	return {"not_found": "Кандидата с таким номером не существует!"}
 
-def get_candidates_by_name(candidates, candidate_name):
+
+def get_candidates_by_name(candidate_name):
 	"""Возвращает кандидата по его имени"""
-	for candidate in candidates:
-		if candidate["name"] == candidate_name:
-			return candidate
+	return [candidate for candidate in candidate_list if candidate_name.lower() in candidate["name"].lower()]
 
 
-def get_candidates_by_skill(candidates, candidate_skill):
+def get_candidates_by_skill(skill_name):
 	"""Возвращает кандидата по его умению"""
-	for candidate in candidates:
-		if candidate["skills"] == candidate_skill:
-			return candidate
+	candidate_skill = []
+
+	for candidate in candidate_list:
+		skills = candidate["skills"].lower().split(", ")
+
+		if skill_name.lower() in skills:
+			candidate_skill.append(candidate)
+
+	return candidate_skill
--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template
 
-from funcs import load_candidates_from_json
+from funcs import load_candidates_from_json, get_candidate, get_candidates_by_name, get_candidates_by_skill
 
 app = Flask(__name__)
+
+candidate_list = load_candidates_from_json("candidates.json")
 
 
 @app.route("/")
 def main_page():
-
-    candidate = load_candidates_from_json("candidates.json")
-
-    return render_template("list.html", candidate=candidate)
+    return render_template("list.html", candidate=candidate_list)
 
 
 @app.route("/candidates/<int:id>")
-def candidates_page():
+def candidates_page(id):
 
-    candidate = load_candidates_from_json("candidates.json")
-    return render_template("card.html", candidate=candidate)
+    cand_id = get_candidate(id)
+
+    return render_template("card.html", candidate=cand_id)
 
 
-app.run()
+@app.route("/search/<name>")
+def candidates_names(name):
+    candidates = get_candidates_by_name(name)
+
+    return render_template("search.html", candidate=candidates, cand_len=len(candidates))
+
+
+@app.route("/skills/<skill_name>")
+def candidates_skill(skill_name):
+    candidates = get_candidates_by_skill(skill_name)
+
+    return render_template("skill.html", candidate=candidates, skill=skill_name, cand_len=len(candidates))
+
+
+if __name__ == "__main__":
+    app.run()
